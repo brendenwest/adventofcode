d8c84371b3d9def73f75de93b0151d9a4c7346f3
Brenden West <brendenw@hotmail.com>
2024-12-01 20:14:59 -0800

    2024, day 1, part 2

--- 2024/day1/answer.main.kts
+++ 2024/day1/answer.main.kts
@@ -7,7 +7,7 @@ import kotlin.math.abs
 val filePath = "input.txt"
 val file = File(filePath)
 
-if (file.exists()) {
+fun part1(file: File) {
     val left = mutableListOf<Int>()
     val right = mutableListOf<Int>()
 
@@ -29,6 +29,35 @@ if (file.exists()) {
     }
 
     println("Part 1 answer: $total")
+
+}
+
+fun part2(file: File) {
+    val left = mutableListOf<Int>()
+    val right = mutableMapOf<Int, Int>()
+
+    // load content into array & map
+    for (line in file.readLines()) {
+        val values = line.split("   ")
+        left.add(values.get(0).toInt())
+        val key = values.get(1).toInt()
+        right[key] = right.getOrDefault(key, 0) + 1
+    }
+
+    var total = 0
+    for (n in 0..left.lastIndex) {
+        val num = left.get(n)
+        val count = right.getOrDefault(num, 0)
+        total += num * count
+    }
+
+    println("Part 2 answer: $total")
+
+}
+
+if (file.exists()) {
+    part1(file)
+    part2(file)
 
 } else {
     println("File not found.")
