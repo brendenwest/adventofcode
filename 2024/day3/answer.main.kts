4271b03ca1cf4c49c8d53d8905ba6fa13510145f
Brenden West <brendenw@hotmail.com>
2024-12-22 14:27:20 -0800

    2024 day3, part2

--- 2024/day3/answer.main.kts
+++ 2024/day3/answer.main.kts
@@ -20,8 +20,30 @@ fun part1(file: File) {
     print("Total: " + sum)
 }
 
+fun part2(file: File) {
+    var sum = 0;
+    var text = file.readText()
+    val regex = """(mul\([0-9]+,[0-9]+\))|(do\(\))|(don't\(\))""".toRegex()
+    val regex2 = """[0-9]+""".toRegex()
+
+    var results = regex.findAll(text)
+    var isValid = true;
+    for (result in results) {
+        if (result.value == "do()") isValid = true;
+        if (result.value == "don't()") isValid = false;
+        if (isValid && result.value.startsWith("mul")) {
+            var nums = regex2.findAll(result.value);
+            sum += nums.first().value.toInt() * nums.last().value.toInt();
+            println(result.value + " " + nums.first().value + " " + nums.last().value)
+        }
+    }
+    print("Total: " + sum)
+
+}
+
 if (file.exists()) {
-    part1(file)
+//    part1(file)
+    part2(file)
 } else {
     println("File not found.")
 }
